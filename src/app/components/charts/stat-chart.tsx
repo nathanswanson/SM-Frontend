5328f8b0235a696608499c0061296adeee8665d0
NS <nathanswanson370@gmail.com>
2025-09-22 13:37:01 -0500

    rever stat chart

--- src/app/components/charts/stat-chart.tsx
+++ src/app/components/charts/stat-chart.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Stat } from '@chakra-ui/react'
+import { HStack, Stat } from '@chakra-ui/react'
 import { Area, AreaChart, ResponsiveContainer, Tooltip } from 'recharts'
 import { convertToGB } from '../../../utils/util'
 
@@ -16,55 +16,45 @@ function dataToRecharts(data: number[][], id: number, expr: (n: number) => numbe
     }))
 }
 
-export const StatChart =
-    ({ metricState, id, label, color, ...rest }: ChartDisplayProps) =>
-    () => {
-        return (
-            <>
-                {metricState.length > 5 ? (
-                    <HStack
-                        marginLeft={4}
-                        justifyItems={'flex-end'}
-                        aspectRatio={2 / 1}
-                        alignItems="center"
-                        height="100%"
-                        width="100%"
-                        {...rest}
-                    >
-                        <Stat.Root>
-                            <Stat.Label>{label}</Stat.Label>
-                            <Stat.ValueText>{convertToGB(metricState[metricState.length - 1][id])}</Stat.ValueText>
-                        </Stat.Root>
-                        <ResponsiveContainer>
-                            <AreaChart
-                                data={dataToRecharts(metricState, id, n => {
-                                    return n
-                                })}
-                                margin={{
-                                    top: 50,
-                                    right: 0,
-                                    left: 0,
-                                    bottom: 8
-                                }}
-                            >
-                                <Area
-                                    type="monotone"
-                                    dataKey="data"
-                                    stroke={color}
-                                    fill={color}
-                                    strokeWidth={2}
-                                    activeDot={false}
-                                    dot={false}
-                                    animationDuration={0}
-                                />
+export const StatChart = ({ metricState, id, label, color, ...rest }: ChartDisplayProps) => (
+    <HStack
+        marginLeft={4}
+        justifyItems={'flex-end'}
+        aspectRatio={2 / 1}
+        alignItems="center"
+        height="100%"
+        width="100%"
+        {...rest}
+    >
+        <Stat.Root>
+            <Stat.Label>{label}</Stat.Label>
+            <Stat.ValueText>{convertToGB(metricState[metricState.length - 1][id])}</Stat.ValueText>
+        </Stat.Root>
+        <ResponsiveContainer>
+            <AreaChart
+                data={dataToRecharts(metricState, id, n => {
+                    return n
+                })}
+                margin={{
+                    top: 50,
+                    right: 0,
+                    left: 0,
+                    bottom: 8
+                }}
+            >
+                <Area
+                    type="monotone"
+                    dataKey="data"
+                    stroke={color}
+                    fill={color}
+                    strokeWidth={2}
+                    activeDot={false}
+                    dot={false}
+                    animationDuration={0}
+                />
 
-                                <Tooltip />
-                            </AreaChart>
-                        </ResponsiveContainer>
-                    </HStack>
-                ) : (
-                    <Box />
-                )}
-            </>
-        )
-    }
+                <Tooltip />
+            </AreaChart>
+        </ResponsiveContainer>
+    </HStack>
+)
